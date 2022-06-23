59d551c228943306de848df21dd2f9c91efe3138
JJH <gjh1996ghooo@gmail.com>
2022-06-23 17:48:45 +0900

    Django3 member join/joinOK 로그인/로그아웃 기능 구현 세션처리, myinfo 기능 구현, 타임존 설정

--- member/urls.py
+++ member/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # 127.0.0.1/join 요청시 member의 join함수가 처리함
     path('join/', views.join, name='join'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('myinfo/', views.myinfo, name='myinfo'),
 ]
--- member/views.py
+++ member/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.hashers import make_password
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from member.models import Member
 
 
+# 회원가입 처리 함수
 def join(request):
     returnPage = 'member/join.html'
 
@@ -41,9 +42,55 @@ def join(request):
         return render(request, returnPage, context)
 
 
+# 로그인 처리 함수
 def login(request):
-    return render(request, 'member/login.html')
+    returnPage = 'member/login.html'
+
+    if request.method == 'GET':
+        return render(request, returnPage)
+
+    elif request.method == 'POST':
+        form = request.POST.dict()
+
+        error = ''
+        if not (form['userid'] and form['passwd']):
+            error = '아이디나 비밀번호가 입력되지 않았어요!'
+        else:
+            try:
+                member = Member.objects.get(userid=form['userid'])
+            except Member.DoesNotExist:
+                member = None
+            if member and check_password(form['passwd'], member.passwd):
+                # 아이디와 비밀번호 인증을 정상적으로 마쳤다면 세션변수에 인증정보를 저장해 둠
+                request.session['userid'] = form['userid']
+
+                return redirect('/')  # index 페이지로 이동
+            else:
+                error = '아이디나 비밀번호가 틀립니다!'
+
+        context = {'error': error}
+        return render(request, returnPage, context)
 
 
+# 로그인 회원의 정보 출력 함수
 def myinfo(request):
-    return render(request, 'member/myinfo.html')
+    member = {}
+    # 로그인 한 회원 아이디를 알아냄 - 세션변수
+    if request.session.get('userid'):
+        userid = request.session.get('userid')
+
+        # 아이디를 이용해서 member 테이블에서 회원정보를 알아냄
+        member = Member.objects.get(userid=userid)
+
+    context = {'member': member}
+    return render(request, 'member/myinfo.html', context)
+
+
+# 로그아웃 처리 함수
+def logout(request):
+    # 만약 세션변수 userid가 존재하면 세션변수 삭제
+    if request.session.get('userid'):
+        del (request.session['userid'])
+
+    # 로그아웃 후 index로 이동
+    return redirect('/')
