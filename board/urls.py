b46b582260adc5fb64207e4035e72c065cd33b87
JJH00101 <107897703+JJH00101@users.noreply.github.com>
2022-06-24 11:58:08 +0900

    Django3 board 모델 생성

--- board/urls.py
+++ board/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('list/', views.list, name='list'),
     path('view/', views.view, name='view'),
-    path('write/', views.write, name='write'),
+    path('/write/', views.write, name='write'),
 ]
