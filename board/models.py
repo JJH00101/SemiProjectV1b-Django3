8525126a0d412784ad53da150e983ac395add359
JJH00101 <107897703+JJH00101@users.noreply.github.com>
2022-06-27 17:34:47 +0900

    Django3 board 리스트/본문 글 출력, 본문 글 수정/삭제하기, 본문 글 조회수 증가, 리스트 no-chache, 본문 글 줄바꿈 적용, 리스트 글 번호 수정

--- board/models.py
+++ board/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from member.models import Member
 from datetime import datetime
+from member.models import Member
 
 
 # Create your models here.
@@ -9,11 +9,10 @@ from datetime import datetime
 class Board(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
-    userid = models.ForeignKey(Member, db_column='userid', on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, related_name='board', on_delete=models.CASCADE)
     regdate = models.DateTimeField(default=datetime.now)
     views = models.IntegerField(default=0)
     contents = models.TextField(null=False, blank=False)
-
 
     class Meta:
         db_table = 'board'
--- board/views.py
+++ board/views.py
@@ -1,23 +1,49 @@
 from django.shortcuts import render, redirect
 from board.models import Board
 from member.models import Member
+from django.db.models import F
 
 
 # Create your views here.
 
 
 def list(request):
-    bdlist = Board.objects.values(
-        'id', 'title', 'userid', 'regdate', 'views'
-    ).order_by('id')
+    # select 'id', 'title', 'userid', 'regdate', 'views'
+    # from board order by id desc
+    #
+    # bdlist = Board.objects.values(
+    #     'id', 'title', 'userid', 'regdate', 'views'
+    # ).order_by('-id')
+
+    # Board와 Member 테이블은 userid <-> id컬럼을 기준으로
+    # inner join을 수행
+    bdlist = Board.objects.select_related('member')
+
+    # join 된 member 테이블의 userid 확인
+    # bdlist.get(0).member.userid
 
     context = {'bds': bdlist}
-
     return render(request, 'board/list.html', context)
 
 
 def view(request):
-    return render(request, 'board/view.html')
+    if request.method == 'GET':
+        form = request.GET.dict()
+
+        b = Board.objects.get(id=form['bno'])
+        b.views = b.views + 1
+        b.save()
+        # Board.objects.filter(id=form['bno']).update(views=F(view('views'))+1)
+
+        # select * from board inner join member
+        # using(id) where id = ???
+        bd = Board.objects.select_related('member').get(id=form['bno'])
+    elif request.method == 'POST':
+        pass
+
+    context = {'bd': bd}
+
+    return render(request, 'board/view.html', context)
 
 
 def write(request):
@@ -37,9 +63,39 @@ def write(request):
                        # 새 글 작성시 작성한 회원에 대한 정보는
                        # 회원테이블에 존재하는 회원번호(id)를 조회해서
                        # userid에 저장
-                       userid=Member.objects.get(pk=form['memberid'])
+                       member=Member.objects.get(pk=form['memberid'])
                        )
             bd.save()
             return redirect('/list')
     context = {'form': form, 'error': error}
     return render(request, returnPage, context)
+
+
+def modify(request):
+    if request.method == 'GET':
+        form = request.GET.dict()
+
+        # select board where bno = ???
+        bd = Board.objects.get(id=form['bno'])
+    elif request.method == 'POST':
+        form = request.POST.dict()
+
+        b = Board.objects.get(id=form['bno'])
+        b.title = form['title']
+        b.contents = form['contents']
+        b.save()
+
+        Board.objects.filter(id=form['bno']).update(title=form['title'], contents=form['contents'])
+
+        return redirect('/view?bno=' + form['bno'])
+    context = {'bd': bd}
+    return render(request, 'board/modify.html', context)
+
+
+def remove(request):
+    if request.method == 'GET':
+        form = request.GET.dict()
+
+        Board.objects.filter(id=form['bno']).delete()
+
+    return None
